f8e7174dbbca5814f9710d1b906ec3af64467d4b
Onkar Pathak <onkarpathak833@gmail.com>
2020-04-07 21:19:49 +0200

    jar file path fix

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -1,3 +1,10 @@
-mkdir image
+#!/bin/bash
+
 cp /jar/spring-cassandra-1.0-SNAPSHOT-application.jar /workspace/build
+FILE=/workspace/build/spring-cassandra-1.0-SNAPSHOT-application.jar
+if [ -f FILE ]; then
+  echo "File exists at destination"
+else
+  echo "File copy failed here"
+fi
 docker build -t gcr.io/project1-186407/spring-cassandra .
