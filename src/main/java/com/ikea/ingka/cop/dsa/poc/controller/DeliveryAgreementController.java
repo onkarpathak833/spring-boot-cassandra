a4997ebac2fe34fa1048987ce7c4dd9e567b98a7
Onkar Pathak <onkarpathak833@gmail.com>
2020-04-07 11:23:04 +0200

    return xml back to request

--- src/main/java/com/ikea/ingka/cop/dsa/poc/controller/DeliveryAgreementController.java
+++ src/main/java/com/ikea/ingka/cop/dsa/poc/controller/DeliveryAgreementController.java
@@ -4,10 +4,29 @@ import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import com.ikea.ingka.cop.dsa.poc.domain.DeliveryAgreementSolution;
 import com.ikea.ingka.cop.dsa.poc.service.DeliveryAgreementService;
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
 
 @Controller
 public class DeliveryAgreementController {
@@ -28,8 +47,17 @@ public class DeliveryAgreementController {
 
     @ResponseBody
     @RequestMapping(value = "/delivery/{id}")
-    public String getDocumentById(@PathVariable String id) {
-        return deliveryService.getDeliveryAgreementSolutionById(id);
+    public String getDocumentById(@PathVariable String id) throws IOException, TransformerException {
+        String jsonString = deliveryService.getDeliveryAgreementSolutionById(id);
+        jsonString = StringEscapeUtils.unescapeJava(jsonString);
+        JSONObject jsonObject = new JSONObject(jsonString);
+        String xmlString = XML.toString(jsonObject);
+        xmlString = xmlString.replaceAll("-", "");
+        System.out.println(xmlString);
+        xmlString = StringEscapeUtils.escapeXml(xmlString);
+        return xmlString;
+
     }
+
 
 }
