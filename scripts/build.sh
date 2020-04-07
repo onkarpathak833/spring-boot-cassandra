7ff001066fa3f83c0fbf9b15120ce63836cb3164
Onkar Pathak <onkarpathak833@gmail.com>
2020-04-07 20:08:13 +0200

    add custom build scripts for cloudbuild

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+gradle clean build customFatJar
+cp /build/libs/spring-cassandra-1.0-SNAPSHOT-application.jar /jar/
+chmod a+x /tmp/spring-cassandra-1.0-SNAPSHOT-application.jar
---
+++ scripts/build_image.sh
@@ -0,0 +1,2 @@
+cp /jar/spring-cassandra-1.0-SNAPSHOT-application.jar /tmp
+docker build -t gcr.io/project1-186407/spring-cassandra .
